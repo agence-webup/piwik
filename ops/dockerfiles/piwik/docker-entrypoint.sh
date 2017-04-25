6d3fae4a91e8637fc70f98986c6c02d8a7d50742
Robin Parisi <parisi.robin@gmail.com>
2017-04-25 13:07:50 +0200

    test

--- ops/dockerfiles/piwik/docker-entrypoint.sh
+++ ops/dockerfiles/piwik/docker-entrypoint.sh
@@ -5,9 +5,7 @@ cd /var/www/html
 
 if [ ! -e piwik.php ]; then
 	cp -ax /usr/src/piwik/* .
-    if [ "$(id -u)" -eq 0 ]; then
-        chown -R www-data .
-    fi
+	chown -R docker .
 fi
 
 exec "$@"
